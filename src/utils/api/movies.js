b0e31ca4c5008968c1da0e7622a54d3256abd9ad
Omar Salama <omarzalama@gmail.com>
2024-05-21 23:04:12 +0300

    moved api endpoint to env file

--- src/utils/api/movies.js
+++ src/utils/api/movies.js
@@ -1,26 +1,27 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const TMDB_URL = import.meta.env.VITE_REACT_TMDB_API_URL;
+const APP_URL = import.meta.env.VITE_REACT_APP_API_URL;
 const token = import.meta.env.VITE_REACT_APP_API_KEY;
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
 export const getPopularMovies = async () => {
-  const movies = await axios.get(`${BASE_URL}/movie/popular`);
+  const movies = await axios.get(`${TMDB_URL}/movie/popular`);
   return movies.data.results;
 }
 
 export const getMovieDetails = async (id) => {
-  const movies = await axios.get(`${BASE_URL}/movie/${id}`);
+  const movies = await axios.get(`${TMDB_URL}/movie/${id}`);
   return movies.data;
 }
 
 export const submitReview = (id, body) => {
-  return axios.post(`http://localhost:3001/movies/${id}/reviews`, body);
+  return axios.post(`${APP_URL}/movies/${id}/reviews`, body);
 }
 
 export const getReviews = async (id) => {
-  const res = await axios.get(`http://localhost:3001/movies/${id}/reviews`);
+  const res = await axios.get(`${APP_URL}/movies/${id}/reviews`);
   return res.data;
 }
 
